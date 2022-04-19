6307398b1e6eb474c76f15c502ec10efe7eb5aa1
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-19 15:17:42 +0200

    Griglia dei film

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     <!-- Main -->
     <main>
 
-      <!-- Sezione da riempire -->
+      <!-- Sezione jumbotron e movies -->
       <section class="bg-black">
 
         <SectionContent />
---
+++ src/components/main/BoxMovie.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="movie">
+        <div class="bg-movie" :style="{ 'background-image': 'url( ' + url +  ' )' } "></div>
+        <p>{{series}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BoxMovie',
+        props: ['url',
+            'series'
+        ]
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../style/variabili.scss';
+    @import '../../style/mixin.scss';
+
+    .movie {
+        width: calc((100% / 6) - 10px);
+        margin: 20px 5px;
+
+        .bg-movie {
+            background-size: cover;
+            width: 100%;
+            aspect-ratio: 1;
+            margin: 10px 0;
+        }
+
+        p {
+            color: white;
+            text-transform: uppercase;
+            font-size: $f-small;
+        }
+
+    }
+</style>
